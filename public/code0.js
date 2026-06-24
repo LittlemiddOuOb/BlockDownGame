3b0362cc8a0459a3ebd64bae47526a2cd7956237
LittlemiddOuOb <kst-23091@kccshatin.edu.hk>
2026-06-24 15:42:48 +0800

    new

--- public/code0.js
+++ public/code0.js
@@ -3,60 +3,107 @@ gdjs.MainCode.localVariables = [];
 gdjs.MainCode.idToCallbackMap = new Map();
 gdjs.MainCode.GDDuo_9595PlaymodeObjects1= [];
 gdjs.MainCode.GDDuo_9595PlaymodeObjects2= [];
+gdjs.MainCode.GDDuo_9595PlaymodeObjects3= [];
 gdjs.MainCode.GDSingle_9595PlaymodeObjects1= [];
 gdjs.MainCode.GDSingle_9595PlaymodeObjects2= [];
+gdjs.MainCode.GDSingle_9595PlaymodeObjects3= [];
 gdjs.MainCode.GDHighestScore2Objects1= [];
 gdjs.MainCode.GDHighestScore2Objects2= [];
+gdjs.MainCode.GDHighestScore2Objects3= [];
 gdjs.MainCode.GDSettingObjects1= [];
 gdjs.MainCode.GDSettingObjects2= [];
+gdjs.MainCode.GDSettingObjects3= [];
 gdjs.MainCode.GDRewardObjects1= [];
 gdjs.MainCode.GDRewardObjects2= [];
+gdjs.MainCode.GDRewardObjects3= [];
 gdjs.MainCode.GDBackToWebButtonObjects1= [];
 gdjs.MainCode.GDBackToWebButtonObjects2= [];
+gdjs.MainCode.GDBackToWebButtonObjects3= [];
 gdjs.MainCode.GDBlueBlockObjects1= [];
 gdjs.MainCode.GDBlueBlockObjects2= [];
+gdjs.MainCode.GDBlueBlockObjects3= [];
 gdjs.MainCode.GDGroundObjects1= [];
 gdjs.MainCode.GDGroundObjects2= [];
+gdjs.MainCode.GDGroundObjects3= [];
 gdjs.MainCode.GDScoreTextObjects1= [];
 gdjs.MainCode.GDScoreTextObjects2= [];
+gdjs.MainCode.GDScoreTextObjects3= [];
 gdjs.MainCode.GDRedBlockObjects1= [];
 gdjs.MainCode.GDRedBlockObjects2= [];
+gdjs.MainCode.GDRedBlockObjects3= [];
 gdjs.MainCode.GDNewSpriteObjects1= [];
 gdjs.MainCode.GDNewSpriteObjects2= [];
+gdjs.MainCode.GDNewSpriteObjects3= [];
 gdjs.MainCode.GDLeftObjects1= [];
 gdjs.MainCode.GDLeftObjects2= [];
+gdjs.MainCode.GDLeftObjects3= [];
 gdjs.MainCode.GDAObjects1= [];
 gdjs.MainCode.GDAObjects2= [];
+gdjs.MainCode.GDAObjects3= [];
 gdjs.MainCode.GDDObjects1= [];
 gdjs.MainCode.GDDObjects2= [];
+gdjs.MainCode.GDDObjects3= [];
 gdjs.MainCode.GDorObjects1= [];
 gdjs.MainCode.GDorObjects2= [];
+gdjs.MainCode.GDorObjects3= [];
 gdjs.MainCode.GDCorrectObjects1= [];
 gdjs.MainCode.GDCorrectObjects2= [];
+gdjs.MainCode.GDCorrectObjects3= [];
 gdjs.MainCode.GDWrongObjects1= [];
 gdjs.MainCode.GDWrongObjects2= [];
+gdjs.MainCode.GDWrongObjects3= [];
 gdjs.MainCode.GDBackObjects1= [];
 gdjs.MainCode.GDBackObjects2= [];
+gdjs.MainCode.GDBackObjects3= [];
 gdjs.MainCode.GDNewSprite2Objects1= [];
 gdjs.MainCode.GDNewSprite2Objects2= [];
+gdjs.MainCode.GDNewSprite2Objects3= [];
 
 
-gdjs.MainCode.asyncCallback13382052 = function (runtimeScene, asyncObjectsList) {
+gdjs.MainCode.eventsList0 = function(runtimeScene, asyncObjectsList) {
+
+{
+
+
+let isConditionTrue_0 = false;
+{
+{runtimeScene.getGame().getVariables().getFromIndex(17).setNumber(gdjs.evtTools.variable.getVariableNumber(runtimeScene.getScene().getVariables().getFromIndex(5).getChild("EasyHighestScore")));
+}
+{runtimeScene.getGame().getVariables().getFromIndex(18).setNumber(gdjs.evtTools.variable.getVariableNumber(runtimeScene.getScene().getVariables().getFromIndex(5).getChild("HellHighestScore")));
+}
+{runtimeScene.getGame().getVariables().getFromIndex(19).setNumber(gdjs.evtTools.variable.getVariableNumber(runtimeScene.getScene().getVariables().getFromIndex(5).getChild("DuoHighestScore")));
+}
+}
+
+}
+
+
+};gdjs.MainCode.eventsList1 = function(runtimeScene, asyncObjectsList) {
+
+{
+
+
+let isConditionTrue_0 = false;
+{
+{gdjs.evtTools.network.jsonToVariableStructure(gdjs.evtTools.variable.getVariableString(runtimeScene.getScene().getVariables().get("ServerResponse")), runtimeScene.getScene().getVariables().getFromIndex(5));
+}
+
+{ //Subevents
+gdjs.MainCode.eventsList0(runtimeScene, asyncObjectsList);} //End of subevents
+}
+
+}
+
+
+};gdjs.MainCode.asyncCallback14639364 = function (runtimeScene, asyncObjectsList) {
 asyncObjectsList.restoreLocalVariablesContainers(gdjs.MainCode.localVariables);
-{runtimeScene.getGame().getVariables().getFromIndex(27).setString("Guest");
-}
-{runtimeScene.getGame().getVariables().getFromIndex(20).getChild("DuoHighestScore").setNumber(gdjs.evtTools.variable.getVariableNumber(runtimeScene.getGame().getVariables().getFromIndex(19)));
-}
-{runtimeScene.getGame().getVariables().getFromIndex(20).getChild("playername").setString(gdjs.evtTools.variable.getVariableString(runtimeScene.getGame().getVariables().getFromIndex(27)));
-}
-{runtimeScene.getGame().getVariables().getFromIndex(20).getChild("HellHighestScore").setNumber(gdjs.evtTools.variable.getVariableNumber(runtimeScene.getGame().getVariables().get("HellHIghestScore")));
-}
-{runtimeScene.getGame().getVariables().getFromIndex(20).getChild("EasyHighestScore").setNumber(gdjs.evtTools.variable.getVariableNumber(runtimeScene.getGame().getVariables().getFromIndex(17)));
-}
+
+{ //Subevents
+gdjs.MainCode.eventsList1(runtimeScene, asyncObjectsList);} //End of subevents
 gdjs.MainCode.localVariables.length = 0;
 }
-gdjs.MainCode.idToCallbackMap.set(13382052, gdjs.MainCode.asyncCallback13382052);
-gdjs.MainCode.eventsList0 = function(runtimeScene) {
+gdjs.MainCode.idToCallbackMap.set(14639364, gdjs.MainCode.asyncCallback14639364);
+gdjs.MainCode.eventsList2 = function(runtimeScene) {
 
 {
 
@@ -65,14 +112,14 @@ gdjs.MainCode.eventsList0 = function(runtimeScene) {
 {
 const asyncObjectsList = new gdjs.LongLivedObjectsList();
 asyncObjectsList.backupLocalVariablesContainers(gdjs.MainCode.localVariables);
-runtimeScene.getAsyncTasksManager().addTask(gdjs.evtTools.network.sendAwaitableAsyncRequest("https://blockdowngame.onrender.com/api/loadData/" + gdjs.evtTools.variable.getVariableString(runtimeScene.getGame().getVariables().getFromIndex(27)), "", "GET", "application/json", runtimeScene.getScene().getVariables().getFromIndex(3), runtimeScene.getScene().getVariables().getFromIndex(4)), (runtimeScene) => (gdjs.MainCode.asyncCallback13382052(runtimeScene, asyncObjectsList)), 13382052, asyncObjectsList);
+runtimeScene.getAsyncTasksManager().addTask(gdjs.evtTools.network.sendAwaitableAsyncRequest("https://blockdowngame.onrender.com/api/loadData/" + gdjs.evtTools.variable.getVariableString(runtimeScene.getGame().getVariables().getFromIndex(27)), "", "GET", "application/json", runtimeScene.getScene().getVariables().getFromIndex(3), runtimeScene.getScene().getVariables().getFromIndex(4)), (runtimeScene) => (gdjs.MainCode.asyncCallback14639364(runtimeScene, asyncObjectsList)), 14639364, asyncObjectsList);
 }
 }
 
 }
 
 
-};gdjs.MainCode.eventsList1 = function(runtimeScene) {
+};gdjs.MainCode.eventsList3 = function(runtimeScene) {
 
 {
 
@@ -89,7 +136,7 @@ if (isConditionTrue_0) {
 }
 
 
-};gdjs.MainCode.eventsList2 = function(runtimeScene) {
+};gdjs.MainCode.eventsList4 = function(runtimeScene) {
 
 {
 
@@ -106,7 +153,7 @@ if (isConditionTrue_0) {
 }
 
 
-};gdjs.MainCode.eventsList3 = function(runtimeScene) {
+};gdjs.MainCode.eventsList5 = function(runtimeScene) {
 
 {
 
@@ -123,9 +170,31 @@ if (isConditionTrue_0) {
 }
 {gdjs.evtTools.runtimeScene.resetTimer(runtimeScene, "BackToWeb");
 }
+{runtimeScene.getGame().getVariables().getFromIndex(27).setString("Guest");
+}
+{runtimeScene.getGame().getVariables().getFromIndex(20).getChild("DuoHighestScore").setNumber(gdjs.evtTools.variable.getVariableNumber(runtimeScene.getGame().getVariables().getFromIndex(19)));
+}
+{runtimeScene.getGame().getVariables().getFromIndex(20).getChild("playername").setString(gdjs.evtTools.variable.getVariableString(runtimeScene.getGame().getVariables().getFromIndex(27)));
+}
+{runtimeScene.getGame().getVariables().getFromIndex(20).getChild("HellHighestScore").setNumber(gdjs.evtTools.variable.getVariableNumber(runtimeScene.getGame().getVariables().get("HellHIghestScore")));
+}
+{runtimeScene.getGame().getVariables().getFromIndex(20).getChild("EasyHighestScore").setNumber(gdjs.evtTools.variable.getVariableNumber(runtimeScene.getGame().getVariables().getFromIndex(17)));
+}
+}
+
+}
+
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+if (isConditionTrue_0) {
 
 { //Subevents
-gdjs.MainCode.eventsList0(runtimeScene);} //End of subevents
+gdjs.MainCode.eventsList2(runtimeScene);} //End of subevents
 }
 
 }
@@ -319,7 +388,7 @@ if (isConditionTrue_0) {
 }
 
 { //Subevents
-gdjs.MainCode.eventsList1(runtimeScene);} //End of subevents
+gdjs.MainCode.eventsList3(runtimeScene);} //End of subevents
 }
 
 }
@@ -341,7 +410,7 @@ if (isConditionTrue_0) {
 }
 
 { //Subevents
-gdjs.MainCode.eventsList2(runtimeScene);} //End of subevents
+gdjs.MainCode.eventsList4(runtimeScene);} //End of subevents
 }
 
 }
@@ -583,82 +652,120 @@ runtimeScene.getOnceTriggers().startNewFrame();
 
 gdjs.MainCode.GDDuo_9595PlaymodeObjects1.length = 0;
 gdjs.MainCode.GDDuo_9595PlaymodeObjects2.length = 0;
+gdjs.MainCode.GDDuo_9595PlaymodeObjects3.length = 0;
 gdjs.MainCode.GDSingle_9595PlaymodeObjects1.length = 0;
 gdjs.MainCode.GDSingle_9595PlaymodeObjects2.length = 0;
+gdjs.MainCode.GDSingle_9595PlaymodeObjects3.length = 0;
 gdjs.MainCode.GDHighestScore2Objects1.length = 0;
 gdjs.MainCode.GDHighestScore2Objects2.length = 0;
+gdjs.MainCode.GDHighestScore2Objects3.length = 0;
 gdjs.MainCode.GDSettingObjects1.length = 0;
 gdjs.MainCode.GDSettingObjects2.length = 0;
+gdjs.MainCode.GDSettingObjects3.length = 0;
 gdjs.MainCode.GDRewardObjects1.length = 0;
 gdjs.MainCode.GDRewardObjects2.length = 0;
+gdjs.MainCode.GDRewardObjects3.length = 0;
 gdjs.MainCode.GDBackToWebButtonObjects1.length = 0;
 gdjs.MainCode.GDBackToWebButtonObjects2.length = 0;
+gdjs.MainCode.GDBackToWebButtonObjects3.length = 0;
 gdjs.MainCode.GDBlueBlockObjects1.length = 0;
 gdjs.MainCode.GDBlueBlockObjects2.length = 0;
+gdjs.MainCode.GDBlueBlockObjects3.length = 0;
 gdjs.MainCode.GDGroundObjects1.length = 0;
 gdjs.MainCode.GDGroundObjects2.length = 0;
+gdjs.MainCode.GDGroundObjects3.length = 0;
 gdjs.MainCode.GDScoreTextObjects1.length = 0;
 gdjs.MainCode.GDScoreTextObjects2.length = 0;
+gdjs.MainCode.GDScoreTextObjects3.length = 0;
 gdjs.MainCode.GDRedBlockObjects1.length = 0;
 gdjs.MainCode.GDRedBlockObjects2.length = 0;
+gdjs.MainCode.GDRedBlockObjects3.length = 0;
 gdjs.MainCode.GDNewSpriteObjects1.length = 0;
 gdjs.MainCode.GDNewSpriteObjects2.length = 0;
+gdjs.MainCode.GDNewSpriteObjects3.length = 0;
 gdjs.MainCode.GDLeftObjects1.length = 0;
 gdjs.MainCode.GDLeftObjects2.length = 0;
+gdjs.MainCode.GDLeftObjects3.length = 0;
 gdjs.MainCode.GDAObjects1.length = 0;
 gdjs.MainCode.GDAObjects2.length = 0;
+gdjs.MainCode.GDAObjects3.length = 0;
 gdjs.MainCode.GDDObjects1.length = 0;
 gdjs.MainCode.GDDObjects2.length = 0;
+gdjs.MainCode.GDDObjects3.length = 0;
 gdjs.MainCode.GDorObjects1.length = 0;
 gdjs.MainCode.GDorObjects2.length = 0;
+gdjs.MainCode.GDorObjects3.length = 0;
 gdjs.MainCode.GDCorrectObjects1.length = 0;
 gdjs.MainCode.GDCorrectObjects2.length = 0;
+gdjs.MainCode.GDCorrectObjects3.length = 0;
 gdjs.MainCode.GDWrongObjects1.length = 0;
 gdjs.MainCode.GDWrongObjects2.length = 0;
+gdjs.MainCode.GDWrongObjects3.length = 0;
 gdjs.MainCode.GDBackObjects1.length = 0;
 gdjs.MainCode.GDBackObjects2.length = 0;
+gdjs.MainCode.GDBackObjects3.length = 0;
 gdjs.MainCode.GDNewSprite2Objects1.length = 0;
 gdjs.MainCode.GDNewSprite2Objects2.length = 0;
+gdjs.MainCode.GDNewSprite2Objects3.length = 0;
 
-gdjs.MainCode.eventsList3(runtimeScene);
+gdjs.MainCode.eventsList5(runtimeScene);
 gdjs.MainCode.GDDuo_9595PlaymodeObjects1.length = 0;
 gdjs.MainCode.GDDuo_9595PlaymodeObjects2.length = 0;
+gdjs.MainCode.GDDuo_9595PlaymodeObjects3.length = 0;
 gdjs.MainCode.GDSingle_9595PlaymodeObjects1.length = 0;
 gdjs.MainCode.GDSingle_9595PlaymodeObjects2.length = 0;
+gdjs.MainCode.GDSingle_9595PlaymodeObjects3.length = 0;
 gdjs.MainCode.GDHighestScore2Objects1.length = 0;
 gdjs.MainCode.GDHighestScore2Objects2.length = 0;
+gdjs.MainCode.GDHighestScore2Objects3.length = 0;
 gdjs.MainCode.GDSettingObjects1.length = 0;
 gdjs.MainCode.GDSettingObjects2.length = 0;
+gdjs.MainCode.GDSettingObjects3.length = 0;
 gdjs.MainCode.GDRewardObjects1.length = 0;
 gdjs.MainCode.GDRewardObjects2.length = 0;
+gdjs.MainCode.GDRewardObjects3.length = 0;
 gdjs.MainCode.GDBackToWebButtonObjects1.length = 0;
 gdjs.MainCode.GDBackToWebButtonObjects2.length = 0;
+gdjs.MainCode.GDBackToWebButtonObjects3.length = 0;
 gdjs.MainCode.GDBlueBlockObjects1.length = 0;
 gdjs.MainCode.GDBlueBlockObjects2.length = 0;
+gdjs.MainCode.GDBlueBlockObjects3.length = 0;
 gdjs.MainCode.GDGroundObjects1.length = 0;
 gdjs.MainCode.GDGroundObjects2.length = 0;
+gdjs.MainCode.GDGroundObjects3.length = 0;
 gdjs.MainCode.GDScoreTextObjects1.length = 0;
 gdjs.MainCode.GDScoreTextObjects2.length = 0;
+gdjs.MainCode.GDScoreTextObjects3.length = 0;
 gdjs.MainCode.GDRedBlockObjects1.length = 0;
 gdjs.MainCode.GDRedBlockObjects2.length = 0;
+gdjs.MainCode.GDRedBlockObjects3.length = 0;
 gdjs.MainCode.GDNewSpriteObjects1.length = 0;
 gdjs.MainCode.GDNewSpriteObjects2.length = 0;
+gdjs.MainCode.GDNewSpriteObjects3.length = 0;
 gdjs.MainCode.GDLeftObjects1.length = 0;
 gdjs.MainCode.GDLeftObjects2.length = 0;
+gdjs.MainCode.GDLeftObjects3.length = 0;
 gdjs.MainCode.GDAObjects1.length = 0;
 gdjs.MainCode.GDAObjects2.length = 0;
+gdjs.MainCode.GDAObjects3.length = 0;
 gdjs.MainCode.GDDObjects1.length = 0;
 gdjs.MainCode.GDDObjects2.length = 0;
+gdjs.MainCode.GDDObjects3.length = 0;
 gdjs.MainCode.GDorObjects1.length = 0;
 gdjs.MainCode.GDorObjects2.length = 0;
+gdjs.MainCode.GDorObjects3.length = 0;
 gdjs.MainCode.GDCorrectObjects1.length = 0;
 gdjs.MainCode.GDCorrectObjects2.length = 0;
+gdjs.MainCode.GDCorrectObjects3.length = 0;
 gdjs.MainCode.GDWrongObjects1.length = 0;
 gdjs.MainCode.GDWrongObjects2.length = 0;
+gdjs.MainCode.GDWrongObjects3.length = 0;
 gdjs.MainCode.GDBackObjects1.length = 0;
 gdjs.MainCode.GDBackObjects2.length = 0;
+gdjs.MainCode.GDBackObjects3.length = 0;
 gdjs.MainCode.GDNewSprite2Objects1.length = 0;
 gdjs.MainCode.GDNewSprite2Objects2.length = 0;
+gdjs.MainCode.GDNewSprite2Objects3.length = 0;
 
 
 return;
